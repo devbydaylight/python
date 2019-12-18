083e3a843c5ff7d98beb301822a1afe72cafa3d0
drpastie <50723211+drpastie@users.noreply.github.com>
2019-12-17 18:01:35 -0600

    Delete exercise_3_1.py

--- thinkPython/exercise_3_1.py
+++
@@ -1,6 +0,0 @@
-# Chapter 3: Functions
-# Exercise 3-1
-#
-# Write a function named right_justify that takes a string named "s" as a parameter and prints the string 
-# with enough leading spaces so that the last letter of the string is in column 70 of the display.
-
